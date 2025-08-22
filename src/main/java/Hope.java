92068b210cd528816924033486c66cf2d8d8ec53
Meckss <maxkhoozhengning1@gmail.com>
2025-08-22 18:40:08 +0800

    Implemented Sets and Hashmaps

--- src/main/java/Hope.java
+++ src/main/java/Hope.java
@@ -1,19 +1,45 @@
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Set;
 
 public class Hope {
+
+    private static boolean online = true;
+    private static final Set<String> COMMANDS = Set.of(
+            "bye"
+    );
+
+    private static final HashMap<String, Command> EXECUTECOMMAND= new HashMap<>();
+
+    static {
+        EXECUTECOMMAND.put("bye", new endCommand());
+    }
+
+    interface Command {
+        void execute();
+    }
+
+    static class endCommand implements Command {
+        @Override
+        public void execute() {
+            System.out.println("See you soon!");
+            online = false;
+        }
+    }
+
     public static void main(String[] args) {
-        boolean online = true;
         System.out.println("Hello! I'm Hope \n" + "What can i do for you? \n");
         while(online) {
             Scanner userInput = new Scanner(System.in);
             String instruction = userInput.nextLine();
-            if(instruction.equals("bye")) {
-                break;
+            if(COMMANDS.contains(instruction)) {
+                EXECUTECOMMAND.get(instruction).execute();
+                if(!online) {
+                    break;
+                }
             }
             System.out.println(instruction +"\n");
         }
-
-        System.out.println("See you soon! \n");
 
     }
 }
