8a45de5171185844d4405cc4be97a26337a71d23
Meckss <maxkhoozhengning1@gmail.com>
2025-08-22 15:54:26 +0800

    Rename Duke to Hope

---
+++ src/main/java/Hope.java
@@ -0,0 +1,6 @@
+public class Hope {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Hope \n" + "What can i do for you? \n");
+        System.out.println("See you soon! \n");
+    }
+}
