afe10270e17d8cb2552d625fac49a055776597c4
Meckss <maxkhoozhengning1@gmail.com>
2025-08-22 17:56:01 +0800

    Implement echo

--- src/main/java/Hope.java
+++ src/main/java/Hope.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Hope {
     public static void main(String[] args) {
+        boolean online = true;
         System.out.println("Hello! I'm Hope \n" + "What can i do for you? \n");
+        while(online) {
+            Scanner userInput = new Scanner(System.in);
+            String instruction = userInput.nextLine();
+            if(instruction.equals("bye")) {
+                break;
+            }
+            System.out.println(instruction +"\n");
+        }
+
         System.out.println("See you soon! \n");
+
     }
 }
