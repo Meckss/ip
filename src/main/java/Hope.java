74218dcefcd7b0dc3aff0f33002fe51fa19c3eac
Meckss <maxkhoozhengning1@gmail.com>
2025-08-22 23:44:47 +0800

    Implement delete command

--- src/main/java/Hope.java
+++ src/main/java/Hope.java
@@ -29,7 +29,8 @@ public class Hope {
             "unmark",
             "todo",
             "deadline",
-            "event"
+            "event",
+            "delete"
     );
 
     private static final HashMap<String, Command> EXECUTECOMMAND= new HashMap<>();
@@ -42,6 +43,7 @@ public class Hope {
         EXECUTECOMMAND.put("todo", new addTDTask());
         EXECUTECOMMAND.put("deadline", new addDTask());
         EXECUTECOMMAND.put("event", new addETask());
+        EXECUTECOMMAND.put("delete", new deleteCommand());
     }
 
     interface Command {
@@ -206,6 +208,39 @@ public class Hope {
                 }
                 System.out.println("Lo! The noble quest, task #" + input + " , doth still beckon thy valorous attention!");
                 System.out.println(toDoList.get(input - 1) + "\n");
+            }
+        }
+    }
+
+    static class deleteCommand implements Command{
+        @Override
+        public void execute(Object o) {
+            if (o instanceof String) {
+                try {
+                    int input = Integer.parseInt((String) o);
+                } catch (NumberFormatException e) {
+                    System.out.println("Pray, employ the noble digits as thy guiding input!\n"
+                            + "(Please use numerics as input only)\n");
+                    return;
+                }
+                int input = Integer.parseInt((String) o);
+                if (input > toDoList.size()) {
+                    System.out.println("Thy request doth stray beyond the hallowed limits.\n"
+                            + "(The number you have input is greater than the number of tasks you currently have)\n");
+                    return;
+                }
+                if (input < 1) {
+                    System.out.println("Doth this be a jest, good sir?\n"
+                            + "(Negative numbers are not accepted as input)\n");
+                    return;
+                }
+
+
+                System.out.println("Heed this decree! This noble quest hath been cast aside.");
+                System.out.println(toDoList.get(input).toString());
+                toDoList.remove(input);
+                System.out.println("Lo! Thou art now bestowed with " + toDoList.size() + " noble quests upon thy parchment of duties.");
+                System.out.println("(You now have " + toDoList.size() + " tasks in the to do list)\n");
             }
         }
     }
