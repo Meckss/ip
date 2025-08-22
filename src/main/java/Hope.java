7e87c4215f532e936e5b17af3389a84626863794
Meckss <maxkhoozhengning1@gmail.com>
2025-08-22 20:02:05 +0800

    Move Task to seperate file

--- src/main/java/Hope.java
+++ src/main/java/Hope.java
@@ -90,34 +90,6 @@ public class Hope {
         }
     }
 
-    static class Task {
-        protected String description;
-        protected boolean isDone;
-
-        public Task(String description) {
-            this.description = description;
-            this.isDone = false;
-        }
-
-        public String getStatusIcon() {
-            return isDone ? "X" : " ";
-        }
-
-        protected void markAsDone() {
-            this.isDone = true;
-        }
-
-        protected void unmark() {
-            this.isDone = false;
-        }
-
-        @Override
-        public String toString() {
-            StringBuilder sb = new StringBuilder();
-            sb.append("[").append(this.getStatusIcon()).append("] ").append(this.description);
-            return sb.toString();
-        }
-    }
 
     public static void main(String[] args) {
         System.out.println("Hello! I'm Hope \n" + "What can i do for you? \n");
