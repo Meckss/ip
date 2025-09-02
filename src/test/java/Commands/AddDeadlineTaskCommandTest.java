9dee987d8b5f89aefbfb64b60f4e66080986bfdd
Meckss <maxkhoozhengning1@gmail.com>
2025-09-02 19:28:16 +0800

    Fix coding standard in AddDeadlineTaskCommandTest.java

--- src/test/java/Commands/AddDeadlineTaskCommandTest.java
+++ src/test/java/Commands/AddDeadlineTaskCommandTest.java
@@ -20,13 +20,15 @@ public class AddDeadlineTaskCommandTest {
         AddDeadlineTaskCommand addDeadlineTaskCommand = new AddDeadlineTaskCommand(toDoList, taskStorage);
         addDeadlineTaskCommand.execute(validInput);
         DeadlineTask expectedOutputToString = new DeadlineTask("test", "02/03/2003");
-        assertEquals(expectedOutputToString.toString(), toDoList.get(1).toString(), "The added deadline task has the correct description and deadline");
+        assertEquals(expectedOutputToString.toString(), toDoList.get(1).toString(), "The added deadline task has the" +
+                " correct description and deadline");
     }
 
     @Test
     public void execute_emptyArgsInput_printErrorMessage() {
         String emptyInput = "deadline";
-        String expectedMessage = "Thou hast overlooked the noble task of bestowing a worthy description upon this endeavor!\n(Looks like you forgot to input a description and a deadline! Try again)\n";
+        String expectedMessage = "Thou hast overlooked the noble task of bestowing a worthy description upon this " +
+                "endeavor!\n(Looks like you forgot to input a description and a deadline! Try again)\n";
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         PrintStream originalOutput = System.out;
         System.setOut(new PrintStream(outputStream));
@@ -37,7 +39,8 @@ public class AddDeadlineTaskCommandTest {
         try {
             addDeadlineTaskCommand.execute(emptyInput);
             String actualOutput = outputStream.toString();
-            assertEquals(expectedMessage.trim(), actualOutput.trim(), "The printed error message for negative input should match.");
+            assertEquals(expectedMessage.trim(), actualOutput.trim(), "The printed error message for negative input " +
+                    "should match.");
         } finally {
             System.setOut(originalOutput);
         }
@@ -45,7 +48,8 @@ public class AddDeadlineTaskCommandTest {
     @Test
     public void execute_noDeadlineInput_printErrorMessage() {
         String noDeadlineInput = "deadline no by";
-        String expectedMessage = "Verily, thou hast erred in thy response; endeavor once more, brave soul!\n(Incorrect input, please try again)\n";
+        String expectedMessage = "Verily, thou hast erred in thy response; endeavor once more, brave soul!\n" +
+                "(Incorrect input, please try again)\n";
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         PrintStream originalOutput = System.out;
         System.setOut(new PrintStream(outputStream));
@@ -56,7 +60,8 @@ public class AddDeadlineTaskCommandTest {
         try {
             addDeadlineTaskCommand.execute(noDeadlineInput);
             String actualOutput = outputStream.toString();
-            assertEquals(expectedMessage.trim(), actualOutput.trim(), "The printed error message for negative input should match.");
+            assertEquals(expectedMessage.trim(), actualOutput.trim(), "The printed error message for negative input " +
+                    "should match.");
         } finally {
             System.setOut(originalOutput);
         }
